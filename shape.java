523a4bfbdde9e591f0c6502af4bd892d48edd8ca
ying718 <55527032+ying718@users.noreply.github.com>
2019-12-08 17:03:33 +0800

    Update shape.java

--- shape.java
+++ shape.java
@@ -1,1 +1,59 @@
 
+
+
+interface Shape
+{
+  abstract void getArea()
+}
+
+class Rectangle implements Shape
+{
+  double length;
+  double width;
+  public Rectangle(double l, double w)
+  {
+    length = l;
+    width = w;
+  }
+
+  public void getArea()
+  {
+    System.out.println("Rectangle ="+length*width);
+  }
+
+  public String toString()
+  {
+  return "Area = ";
+  }
+
+}
+
+class Triangle implements Shape
+{
+  double base;
+  double height;
+  public Triangle(double b, double h)
+  {
+    base = b;
+    height = h;
+  }
+
+  public void getArea()
+  {
+    System.out.println("Triangle ="+base*height/2);
+  }
+  public String toString (){
+    return "Area = ";
+  }
+}
+
+public class app11
+{
+	public static void main (String args[])
+	{
+		Rectangle rec = new Rectangle(10,20);
+		System.out.println(rec);
+		Triangle tri = new Triangle(10,20):
+		System.out.println(tri);
+	}
+}
